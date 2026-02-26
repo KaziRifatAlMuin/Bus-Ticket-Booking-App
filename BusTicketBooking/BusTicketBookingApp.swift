f2ab7161ceccd535038f42f7e7267937dd3ff329
Kazi Rifat Al Muin <kazirifatalmuin246@gmail.com>
2026-02-26 19:10:39 +0600

    Swift Project Created

---
+++ BusTicketBooking/BusTicketBookingApp.swift
@@ -0,0 +1,17 @@
+//
+//  BusTicketBookingApp.swift
+//  BusTicketBooking
+//
+//  Created by macos on 26/2/26.
+//
+
+import SwiftUI
+
+@main
+struct BusTicketBookingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
